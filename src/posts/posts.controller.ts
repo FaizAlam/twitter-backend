b43d215e370e6c231524e05346c5bded18d76f54
Faiz <mohdfaizalam53@gmail.com>
2021-09-20 13:29:53 +0530

    add posts controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+    @Get('/')
+    getAllPost():string{
+        return 'get all posts';
+    }
+
+    @Get('/:postid')
+    getPostDetails(@Param() param):string{
+        return `details of postid = ${param.postid}`;
+    }
+
+    @Post('/')
+    createNewPost():string{
+        //TODO
+        return `new post created`;
+    }
+
+    @Delete('/:postid')
+    deletePost(@Param() param):string{
+        //TODO
+        return `post deleted = ${param.postid}`;
+    }
+
+    @Put('/:postid/like')
+    likePost(@Param() param):string{
+        //TODO
+        return `liked post ${param.postid}`;
+    }
+    
+    @Delete('/:postid/like')
+    unlikePost(@Param() param):string{
+        //TODO
+        return `unliked post ${param.postid}`;
+    }
+
+
+    
+}
