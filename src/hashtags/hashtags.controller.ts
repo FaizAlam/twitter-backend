b0870f9cbce4c97c549048f75dedfe504ecae3cd
Faiz <mohdfaizalam53@gmail.com>
2021-09-20 12:00:36 +0530

    Added controller for hashtags and posts and users

---
+++ src/hashtags/hashtags.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('hashtags')
+export class HashtagsController {
+
+    @Get("/")
+    getHashtag():string{
+        //TODO: add actual logic
+        return 'All top hashtag';
+    }
+
+    @Get("/:tag/posts")
+    getPostsForHashtag(@Param() param):string{
+        return `show all posts with hashtag ${param.tag}`;
+    }
+    
+}
